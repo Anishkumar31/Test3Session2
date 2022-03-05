81f5dc7cd63433eba91c0d15f76f454ef580be76
Anish kumar <94589270+Anishkumar31@users.noreply.github.com>
2022-03-05 06:36:31 +0000

    p1 done

--- p1final.c
+++ p1final.c
@@ -1,23 +1,30 @@
 #include<stdio.h>
+
 void input(int *num1,int *den1,int *num2,int *den2)
 {
-  printf("Enter the fraction number");
+  printf("Enter the fraction value\n");
   scanf("%d%d%d%d",num1,den1,num2,den2);
 }
 
 void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
 {
-  int c;
-  
-  c=den1*den2;
-  
-  *num3=(num1*den2)+(num2*den1);
-  *den3=(c); 
+  int x,y,gcd;
+  x=((num1*den2)+(num2*den1));
+  y=den1*den2;
+  for(int i=1;i<=x && i<=y;++i)
+    {
+      if(x%i==0 && y%i==0)
+      {
+        gcd=i;
+      }
+    }
+  *num3=x/gcd;
+  *den3=y/gcd;
 }
 
-void output(int num1,int den1,int num2,int den2,int num3,int den3)
+void output(int num3,int den3)
 {
-  printf("addition of %d/%d + %d/%d = %d/%d",num1,den1,num2,den2,num3,den3);
+  printf("addition is %d/%d",num3,den3);
 }
 
 int main()
@@ -25,7 +32,6 @@ int main()
   int num1,den1,num2,den2,num3,den3;
   input(&num1,&den1,&num2,&den2);
   add(num1,den1,num2,den2,&num3,&den3);
-  output(num1,den1,num2,den2,num3,den3);
+  output(num3,den3);
   return 0;
 }
- 
--- p2final.c
+++ p2final.c
@@ -1,1 +1,12 @@
 #include<stdio.h>
+struct _fraction
+{
+int num,den;
+printf("enter the numerator\n");
+scanf("%d",&num);
+printf("enter the denominator\n");
+scanf("%d",&den);
+};
+typedef struct _ fraction Fraction
+Fraction input()
+  
